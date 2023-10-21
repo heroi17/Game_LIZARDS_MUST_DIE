95c3f6fae49aca7c69c983820c7f5857acdcdfee
Dmitry-klimovski <dimasikklimovski35@gmail.com>
2023-10-21 17:04:45 +0300

    added fields

--- Entity.h
+++ Entity.h
@@ -1,12 +1,18 @@
 #ifndef LMD_GAMELOGIC_ENTITY
 #define LMD_GAMELOGIC_ENTITY
 
+#include <vector>
+
 class Entity
 {
 public:
+	int Speed;
+	int MaxHealth;
+	int Health;
+	int Damage;
+	std::vector<int> Velocity;
 
 private:
-
 };
 
 #endif // !LMD_GAMELOGIC_ENTITY
