78cb9c0004de9678bdeb7a1e6d627a34be589ac0
Dmitry-klimovski <dimasikklimovski35@gmail.com>
2023-10-21 13:14:56 +0300

    Fields added

--- Item.h
+++ Item.h
@@ -1,12 +1,19 @@
 #ifndef LMD_GAMELOGIC_ITEM
 #define LMD_GAMELOGIC_ITEM
 
+#include <string>
+
 class Item
 {
 public:
+	
+	Item();
+	Item(std::string name, std::string discription);
+	virtual void ApplyEffect();
 
 private:
-
+	std::string _name;
+	std::string _discription;
 };
 
 #endif // !LMD_GAMELOGIC_ITEM
