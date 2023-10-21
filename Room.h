ba8d566ae39bc97bf78602cedd7b7ac0e1af39eb
Dmitry-klimovski <dimasikklimovski35@gmail.com>
2023-10-21 17:04:33 +0300

    added fields

--- Room.h
+++ Room.h
@@ -1,12 +1,20 @@
 #ifndef LMD_GAMELOGIC_ROOM
 #define LMD_GAMELOGIC_ROOM
 
+#include <vector>
+
+#include "Item.h"
+#include "Entity.h"
+
 class Room
 {
 public:
+	Room();
+	Room(std::vector<Entity> enemies, Item rewardItem);
 
 private:
-
+	std::vector<Entity> _enemies;
+	Item _rewardItem;
 };
 
 #endif // !LMD_GAMELOGIC_ROOM
